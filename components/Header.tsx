bcab17937559ce77dce69130336d29d4e2e0859c
ihtisham914 <shaam.khn321@gmail.com>
2024-07-20 17:21:22 +0500

    added  editor and header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+const Header = ({ children }: HeaderProps) => {
+    return (
+        <div className='header'>
+            <Link href="/" className='md:flex-1'>
+                <Image src='/assets/icons/logo.svg' alt="logo" width={120} height={32} className='hidden md:block' />
+                <Image src='/assets/icons/logo-icon.svg' alt="logo" width={32} height={32} className='mr-2 md:hidden' />
+            </Link>
+            {children}
+        </div>
+    )
+}
+
+export default Header
