0918ccd7285796ddf4dae487a8e5563df511c12b
ihtisham914 <shaam.khn321@gmail.com>
2024-07-24 13:00:17 +0500

    added updateDocument title feature

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -47,3 +47,17 @@ export const getDocument = async ({ roomId, userId }: { roomId: string; userId: 
         console.log(error)
     }
 }
+
+export const updateDocument = async (roomId: string, title: string) => {
+    try {
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            metadata: {
+                title
+            }
+        })
+        revalidatePath(`/documents/${roomId}`)
+        return parseStringify(updatedRoom)
+    } catch (error) {
+        console.log(error)
+    }
+}
