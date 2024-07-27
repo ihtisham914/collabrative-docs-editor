2dcd431405aa756f4e79575586e4ab486da32b36
ihtisham914 <shaam.khn321@gmail.com>
2024-07-27 21:29:24 +0500

    added mention feature

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,8 @@
 'use server'
 
-import { clerkClient, EmailAddress } from "@clerk/nextjs/server"
+import { clerkClient } from "@clerk/nextjs/server"
 import { parseStringify } from "../utils"
+import { liveblocks } from "../liveblocks"
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     try {
@@ -20,4 +21,21 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     } catch (error) {
         console.log(error)
     }
-}   
+}
+
+export const getDocumentUsers = async ({ roomId, currentUser, text }: { roomId: string, currentUser: string, text: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId)
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser)
+        if (text.length) {
+            const lowerCaseText = text.toLowerCase()
+
+            const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText))
+
+            return parseStringify(filteredUsers)
+        }
+        return parseStringify(users)
+    } catch (error) {
+        console.log(error)
+    }
+}
