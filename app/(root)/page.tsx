29b2fed106275f90c9f0bddb8fff361b28e9f0ae
ihtisham914 <shaam.khn321@gmail.com>
2024-07-29 16:14:39 +0500

    notifications implemented for all use cases

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,7 @@ import { getDocuments } from '@/lib/actions/room.actions'
 import Link from 'next/link'
 import { dateConverter } from '@/lib/utils'
 import { DeleteModal } from '@/components/DeleteModal'
+import Notifications from '@/components/Notifications'
 
 
 const Home = async () => {
@@ -19,7 +20,7 @@ const Home = async () => {
     <main className='home-container'>
       <Header className='sticky left-0 top-0'>
         <div className='flex items-center gap-2 lg:gap-4'>
-          Notifications
+          <Notifications />
           <SignedIn>
             <UserButton />
           </SignedIn>
