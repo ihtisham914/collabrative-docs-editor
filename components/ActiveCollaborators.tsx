27d195f55de17c7a055fccad43f6cd5d4be3a456
ihtisham914 <shaam.khn321@gmail.com>
2024-07-29 16:34:29 +0500

    updated css

--- components/ActiveCollaborators.tsx
+++ components/ActiveCollaborators.tsx
@@ -7,7 +7,7 @@ const ActiveCollaborators = () => {
 
     const collaborators = others.map((others) => others.info)
     return (
-        <ul className='collaborators-;ost'>
+        <ul className='collaborators-list'>
             {collaborators.map(({ id, name, avatar, color }) => <li key={id}>
                 <Image src={avatar} alt={name} width={100} height={100} className='inline-block size-8 rounded-full ring-2' style={{ border: `3px solid ${color}` }} />
             </li>)}
